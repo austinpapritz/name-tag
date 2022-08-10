77f51ac97c1228b3d8c9fac452d5fedc586c6980
Austin Papritz <austin@papritz.dev>
2022-08-09 19:54:31 -0700

    buttons labels and name tag

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-// import functions and grab DOM elements
+// grab DOM elements
+const nameInput = document.getElementById('name');
+const animalSelect = document.getElementById('animal-select');
+const imageInput = document.getElementById('image-input');
 
-// let state
+const nameDisplay = document.getElementById('name-display');
+const animalDisplay = document.getElementById('animal-display');
+const imageDisplay = document.getElementById('image-display');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// add event listeners...
+
+nameInput.addEventListener('input', () => {
+    // get name input value
+    // update DOM to reflect the new state
+    nameDisplay.textContent = nameInput.value;
+});
+
+animalSelect.addEventListener('change', () => {
+    // get select input value and
+    // update DOM to reflect this
+    animalDisplay.textContent = animalSelect.value;
+});
+
+imageInput.addEventListener('change', () => {
+    // get the file from the upload input
+    const file = imageInput.files[0];
+    // convert the image into something we can
+    // directly assign to the .src property of the image,
+    // using a builtin utility for this purpose
+    imageDisplay.src = URL.createObjectURL(file);
+});
